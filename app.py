3db568d751c06ffa9765c9ff85d9fd9241dcb763
KashiwaByte <471314513@qq.com>
2023-09-13 10:37:23 +0000

    pyttxs driver espeak

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import openai, subprocess
 from pathlib import Path
 import pyttsx3
 
+engine = pyttsx3.init(driverName='espeak')
 # 换成你自己的api_key
 openai.api_key = "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"
 debatestyle = " "
