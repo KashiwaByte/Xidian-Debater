521621df4250f6c01fee7662d9ca71d22107ff80
黄柏特 <471314513@qq.com>
2023-09-14 09:13:16 +0800

    change tts to Azure 1

--- app.py
+++ app.py
@@ -2,18 +2,33 @@
 import gradio as gr
 import openai, subprocess
 from pathlib import Path
-import pyttsx3
+import os
+import azure.cognitiveservices.speech as speechsdk
+
+
+# Azure tts
+# This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+SPEECH_REGION='eastus'
+SPEECH_KEY= 'af2d4aa2348b4b73b60487c73e0eb431'
+
+speech_config = speechsdk.SpeechConfig(subscription='af2d4aa2348b4b73b60487c73e0eb431', region='eastus')
+audio_config = speechsdk.audio.AudioOutputConfig(use_default_speaker=True)
+
+# The language of the voice that speaks.
+speech_config.speech_synthesis_voice_name='	zh-CN-XiaoxiaoNeural'
+
+speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
 
 # 换成你自己的api_key
-openai.api_key = "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"
+openai.api_key = "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"
 debatestyle = " "
 messages = [{"role": "system", "content": '你是一名逻辑性很强的资深辩手，你擅长通过数据论据和学理论据来反驳，你的反驳总是一阵见血，而且很有逻辑性。接下来我会提出我的观点，你需要做的就是针锋相对地反驳我'}]
 chat_transcript = ""
 def transcribe(audio):
     global messages
     global chat_transcript
-
+    global speech_synthesizer
     myfile=Path(audio)
     myfile=myfile.rename(myfile.with_suffix('.wav'))
     audio_file = open(myfile,"rb")
@@ -27,9 +42,9 @@ def transcribe(audio):
     # print(response)
     messages.append(system_message)
 
-
-
-   # pyttsx3.speak(system_message['content'])
+    speech_synthesis_result = speech_synthesizer.speak_text_async(system_message['content']).get()
+    
+    # 原windows tts 解决方案  subprocess.call(["wsay", system_message['content']])
 
     chat_transcript = ""
     for message in messages:
@@ -54,7 +69,7 @@ def initway(api_key,style):
 with gr.Blocks(css="#chatbot{height:300px} .overflow-y-auto{height:500px}") as init:
     with gr.Row():
         api_key = gr.Textbox(
-            lines=1, placeholder="api_key Here...", label="api_key",value="")
+            lines=1, placeholder="api_key Here...", label="api_key",value="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")
         style = gr.Textbox(
             lines=1, placeholder="style Here...", label="辩风" ,value="你是一名逻辑性很强的资深辩手，你擅长通过数据论据和学理论据来反驳，你的反驳总是一阵见血，而且很有逻辑性。接下来我会提出我的观点，你需要做的就是针锋相对地反驳我，每次回答不超过100个字")
         btn = gr.Button(value="初始化")
